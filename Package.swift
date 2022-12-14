3087d315be7a72fc241622f8bdf6e26afbeab512
Jeonhui <dlwjsgml02@naver.com>
2022-12-15 03:16:17 +0900

    feat: update dependencies

--- Package.swift
+++ Package.swift
@@ -14,15 +14,19 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FirebaseCombine",
-            dependencies: []),
+            dependencies: [.product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                           .product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk"),
+                           .product(name: "FirebaseDatabase", package: "firebase-ios-sdk")]),
         .testTarget(
             name: "FirebaseCombineTests",
             dependencies: ["FirebaseCombine"]),
     ]
+    
 )
